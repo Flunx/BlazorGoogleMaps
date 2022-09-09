cf78aebdf1a3d6f7b8f3a612d622be16260a652d
Stian Dahl <stian.dahl@tracab.com>
2022-09-09 09:51:53 +0200

    Added Parse method to the LatLngLiteral

--- GoogleMapsComponents/Maps/Coordinates/LatLngLiteral.cs
+++ GoogleMapsComponents/Maps/Coordinates/LatLngLiteral.cs
@@ -40,5 +40,13 @@ namespace GoogleMapsComponents.Maps
         public LatLngLiteral(decimal lat, decimal lng) : this(Convert.ToDouble(lat), Convert.ToDouble(lng))
         {
         }
+
+        public static LatLngLiteral Parse(string literal)
+        {
+            var s = literal.Split(',');
+            var lat = double.Parse(s[0], System.Globalization.CultureInfo.InvariantCulture);
+            var lng = double.Parse(s[1], System.Globalization.CultureInfo.InvariantCulture);
+            return new LatLngLiteral(lat, lng);
+        }
     }
 }
